cc1e8a7e573bbfa9bfe3584d7d42bd36a34b0b7e
ShriramMapalakaje <108290635+ShriramMapalakaje@users.noreply.github.com>
2026-02-05 13:36:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+// 1. Use the location object to get the current URL
+const currentURL = window.location.href;
+
+// 2. Use the length property to count the letters
+const urlLength = currentURL.length;
+
+// 3. Use alert() to print the final length in the specific format
+alert("The length of the URL is: " + urlLength);
